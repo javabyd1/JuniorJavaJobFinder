a0e20dd79b570b5e29b0a4db2271c791b8a1c790
PawelOrzel <bassista@interia.pl>
2018-01-28 14:11:37 +0100

    add repository, service

---
+++ src/main/java/com/sda/juniorjavajobfinder/groupproject/service/CompanyServiceImpl.java
@@ -0,0 +1,36 @@
+package com.sda.juniorjavajobfinder.groupproject.service;
+
+import com.sda.juniorjavajobfinder.groupproject.interfaces.CompanyService;
+import com.sda.juniorjavajobfinder.groupproject.model.Company;
+import com.sda.juniorjavajobfinder.groupproject.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyServiceImpl implements CompanyService{
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Override
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @Override
+    public Company createCompany(Company company) {
+        return companyRepository.save(company);
+    }
+
+    @Override
+    public Company updateCompany(Company company) {
+        return companyRepository.save(company);
+    }
+
+    @Override
+    public void deleteCompany(Company company) {
+        companyRepository.delete(company);
+    }
+}
