1a8efa8ca22c01992b99940d262eab9988f45594
AhlenSDA <ahlensda@wp.pl>
2018-01-28 12:54:47 +0100

    Repository initialized with SrpingBoot

---
+++ src/main/java/com/sda/juniorjavajobfinder/groupproject/GroupProjectApplication.java
@@ -0,0 +1,12 @@
+package com.sda.juniorjavajobfinder.groupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupProjectApplication.class, args);
+	}
+}
