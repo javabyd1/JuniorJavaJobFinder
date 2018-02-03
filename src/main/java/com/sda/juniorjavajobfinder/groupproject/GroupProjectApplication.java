15a05db5799191f16d7be040aa31085510641c7f
Dariusz <rozik85@o2.pl>
2018-02-03 15:48:39 +0100

    add butto search

--- src/main/java/com/sda/juniorjavajobfinder/groupproject/GroupProjectApplication.java
+++ src/main/java/com/sda/juniorjavajobfinder/groupproject/GroupProjectApplication.java
@@ -1,6 +1,8 @@
 package com.sda.juniorjavajobfinder.groupproject;
 
+import com.sda.juniorjavajobfinder.groupproject.interfaces.AnnoucementService;
 import com.sda.juniorjavajobfinder.groupproject.model.Annoucement;
+import com.sda.juniorjavajobfinder.groupproject.model.City;
 import com.sda.juniorjavajobfinder.groupproject.model.Company;
 import com.sda.juniorjavajobfinder.groupproject.service.CompanyServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,26 +10,34 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 @SpringBootApplication
 public class GroupProjectApplication implements CommandLineRunner {
 
-	@Autowired
-	CompanyServiceImpl companyService;
+    @Autowired
+    CompanyServiceImpl companyService;
+    @Autowired
+    AnnoucementService annoucementService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(GroupProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GroupProjectApplication.class, args);
+
+    }
 
 
-	@Override
-	public void run(String... strings) throws Exception {
+    @Override
+    public void run(String... strings) throws Exception {
 
-		Company company = new Company();
-		company.setCity("Bydgoszcz");
-		company.setName("Atos");
-		companyService.createCompany(company);
+        Company company = new Company();
+        company.setCity("Bydgoszcz");
+        company.setName("Atos");
+        companyService.createCompany(company);
 
-		Annoucement annoucement = new Annoucement();
-		System.out.println(annoucement.getDate());
-	}
+        for (Annoucement annoucement : annoucementService.getAllAnnoucement()) {
+            annoucement.setDate(LocalDate.now());
+            annoucementService.updateAnnoucement(annoucement);
+        }
+    }
 }
