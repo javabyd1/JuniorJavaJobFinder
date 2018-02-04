97c4c5c5c955debdebdf38d12049c2e56363dcef
AhlenSDA <ahlensda@wp.pl>
2018-02-04 15:02:53 +0100

    Add methods to classes
    - add UserService methods
    - add UserServiceImpl methods

--- src/main/java/com/sda/juniorjavajobfinder/groupproject/service/AnnouncementServiceImpl.java
+++ src/main/java/com/sda/juniorjavajobfinder/groupproject/service/AnnouncementServiceImpl.java
@@ -2,17 +2,17 @@ package com.sda.juniorjavajobfinder.groupproject.service;
 
 import com.sda.juniorjavajobfinder.groupproject.interfaces.AnnoucementService;
 import com.sda.juniorjavajobfinder.groupproject.model.Announcement;
-import com.sda.juniorjavajobfinder.groupproject.repository.AnnoucementRepository;
+import com.sda.juniorjavajobfinder.groupproject.repository.AnnouncementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class AnnoucementServiceImpl implements AnnoucementService {
+public class AnnouncementServiceImpl implements AnnoucementService {
 
     @Autowired
-    private AnnoucementRepository annoucementRepository;
+    private AnnouncementRepository annoucementRepository;
 
     @Override
     public List<Announcement> getAllAnnoucement() {
--- src/main/java/com/sda/juniorjavajobfinder/groupproject/service/UserServiceImpl.java
+++ src/main/java/com/sda/juniorjavajobfinder/groupproject/service/UserServiceImpl.java
@@ -1,4 +1,24 @@
 package com.sda.juniorjavajobfinder.groupproject.service;
 
-public class UserServiceImpl {
+import com.sda.juniorjavajobfinder.groupproject.interfaces.UserService;
+import com.sda.juniorjavajobfinder.groupproject.model.User;
+import com.sda.juniorjavajobfinder.groupproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
 }
