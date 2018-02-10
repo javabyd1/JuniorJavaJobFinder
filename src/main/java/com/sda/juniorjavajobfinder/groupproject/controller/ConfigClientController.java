db366faba7af5c93120a528cb1125475470fb703
Bartosz Jańczak <b.janczak90@gmail.com>
2018-02-10 21:01:54 +0100

    cloud properties

--- src/main/java/com/sda/juniorjavajobfinder/groupproject/controller/ConfigClientController.java
+++ src/main/java/com/sda/juniorjavajobfinder/groupproject/controller/ConfigClientController.java
@@ -2,27 +2,19 @@ package com.sda.juniorjavajobfinder.groupproject.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
 public class ConfigClientController {
 
-    @Value("${info.property}")
-    private String property;
-
-    @Value("${info.encrypted.property}")
-    private String encrypted;
-
-    @GetMapping("/message")
-    String property() {
-        return property;
-    }
-
-    @GetMapping("/decrypted")
-    String decrypted() {
-        return encrypted;
-    }
+//    @Value("${message}")
+//    private String message;
+//
+//    @RequestMapping("/message")
+//    String getMessage() {
+//        return this.message;
+//    }
 
 }
