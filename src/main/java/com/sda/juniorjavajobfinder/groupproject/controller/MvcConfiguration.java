d3c38beb4cec92e29bd00ad31cc07519905579a6
AhlenSDA <ahlensda@wp.pl>
2018-02-10 12:30:25 +0100

    Add MvC view configuration

---
+++ src/main/java/com/sda/juniorjavajobfinder/groupproject/controller/MvcConfiguration.java
@@ -0,0 +1,17 @@
+package com.sda.juniorjavajobfinder.groupproject.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/announcement").setViewName("announcement");
+        registry.addViewController("/company").setViewName("company");
+        registry.addViewController("/home").setViewName("home");
+    }
+}
