d310c84a571568168f162dbfc2fc966559336533
Bartek <b.janczak90@gmail.com>
2018-02-11 11:21:49 +0100

    working cloud properties

--- src/main/java/com/sda/juniorjavajobfinder/groupproject/GroupProjectApplication.java
+++ src/main/java/com/sda/juniorjavajobfinder/groupproject/GroupProjectApplication.java
@@ -1,23 +1,14 @@
 package com.sda.juniorjavajobfinder.groupproject;
 
 import com.sda.juniorjavajobfinder.groupproject.interfaces.AnnouncementService;
-import com.sda.juniorjavajobfinder.groupproject.model.Announcement;
 import com.sda.juniorjavajobfinder.groupproject.service.CompanyServiceImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class GroupProjectApplication implements CommandLineRunner {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(GroupProjectApplication.class);
 
     @Autowired
     CompanyServiceImpl companyService;
@@ -32,9 +23,5 @@ public class GroupProjectApplication implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
 
-        for (Announcement announcement : annoucementService.getAllAnnouncement()) {
-            announcement.setDate(LocalDate.now());
-            annoucementService.updateAnnouncement(announcement);
-        }
     }
 }
